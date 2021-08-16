b772dbbf44aef81a3fda19444c0860dac57e6ae4
Jose Neyra <jose-neyra@outlook.com>
2021-08-16 10:37:10 -0400

    Completed Intro Section: JPA-Entries, Data-Repos, H2-Database-Console, Spring-MVC, Controllers, Thymeleaf-Templates

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -0,0 +1,27 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        // This tells the Spring Controller that we want to use the template under the directory books called list
+        // to display this controller.
+        return "books/list";
+    }
+}
